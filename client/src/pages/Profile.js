deb230b1f59ff984fa897f1f98ccb3d421b23e3c
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-07-01 12:46:00 -0500

    Updating current state of the application to finish work on it

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -7,11 +7,11 @@ import FriendList from '../components/FriendList';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 
-const Profile = () => {
+const Profile = (props) => {
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(QUERY_USER, {
-    variables: { username: userParam }
+    variables: { username: userParam },
   });
 
   const user = data?.user || {};
@@ -29,18 +29,21 @@ const Profile = () => {
       </div>
 
       <div className="flex-row justify-space-between mb-3">
-  <div className="col-12 mb-3 col-lg-8">
-    <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
-  </div>
+        <div className="col-12 mb-3 col-lg-8">
+          <ThoughtList
+            thoughts={user.thoughts}
+            title={`${user.username}'s thoughts...`}
+          />
+        </div>
 
-  <div className="col-12 col-lg-3 mb-3">
-    <FriendList
-      username={user.username}
-      friendCount={user.friendCount}
-      friends={user.friends}
-    />
-  </div>
-</div>
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
+        </div>
+      </div>
     </div>
   );
 };
